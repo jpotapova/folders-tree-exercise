11a3da155ac3bc5e191b21ff660913ae77ba1d1a
Jelizaveta Potapova <potapova.jelizaveta@gmail.com>
2020-09-03 23:14:30 +0300

    Add prod build

---
+++ build/precache-manifest.18285af204d5a8ca6e0a5808e241507d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7ba4b27c8136abc93fe4eab12d8470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5de1f19fe967d31a9d9a",
+    "url": "/static/css/main.c4c2eb76.chunk.css"
+  },
+  {
+    "revision": "8e350320996b803b8b25",
+    "url": "/static/js/2.9866ce6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9866ce6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de1f19fe967d31a9d9a",
+    "url": "/static/js/main.b33f6bf1.chunk.js"
+  },
+  {
+    "revision": "3978072e5b9fbcb200a6",
+    "url": "/static/js/runtime-main.a6f3e246.js"
+  }
+]);
